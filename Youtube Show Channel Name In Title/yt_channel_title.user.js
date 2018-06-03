f396b65b0a3dbe53818ac4e45ac5b55176d697a4
AleksT <37518707+tkhquang@users.noreply.github.com>
2018-06-04 00:35:09 +0700

    Update yt_channel_title.user.js

--- Youtube Show Channel Name In Title/yt_channel_title.user.js
+++ Youtube Show Channel Name In Title/yt_channel_title.user.js
@@ -1,41 +1,42 @@
 // ==UserScript==
 // @name         Youtube Show Channel Name In Title
 // @namespace    https://github.com/tkhquang
-// @version      1.0
+// @version      1.1
 // @description  Show channel's name (username) in title page
 // @author       Aleks
 // @homepage     https://greasyfork.org/en/scripts/368421-youtube-show-channel-name-in-title
-// @match        https://www.youtube.com/*
+// @match        http*://www.youtube.com/*
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
 
+if (!String.prototype.trim) {
+    String.prototype.trim = function () {
+        return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, "");
+    };
+}
+
 (function() {
     "use strict";
 
-    var channelName;
-
+    var channelName, observer = new MutationObserver(setTitle);
     function setTitle() {
-        if (document.querySelector('#owner-name') === null) {
-            setTimeout(setTitle, 5000);
+        if (!document.getElementById("owner-name")) {
+            setTimeout(setTitle, 1000);
             return;
         }
-        channelName = document.getElementById("owner-name").textContent;
+        channelName = document.getElementById("owner-name").textContent.trim();
         if (document.title.startsWith(channelName + " | ")) return;
         document.title = channelName + " | " + document.title;
     }
-
-    var observer = new MutationObserver(setTitle);
-    var config = {
-        childList: true,
-        attributes: false,
-        characterData: false,
-        subtree: false
-    };
-
     document.addEventListener("yt-navigate-finish", function () {
-        if (/^\/watch?/.test(window.location.pathname) === true) {
-            observer.observe(document.getElementsByTagName("title")[0], config);
+        if (/^\/watch?/.test(window.location.pathname)) {
+            observer.observe(document.getElementsByTagName("title")[0], {
+                childList: true,
+                attributes: false,
+                characterData: false,
+                subtree: false
+            });
         }
         else {
             observer.disconnect();
