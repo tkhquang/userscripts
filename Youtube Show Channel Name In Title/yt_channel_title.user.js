c220dae29f9f405d4e2b86c16433987e093b6a47
AleksT <37518707+tkhquang@users.noreply.github.com>
2018-06-04 08:55:54 +0700

    Update yt_channel_title.user.js

--- Youtube Show Channel Name In Title/yt_channel_title.user.js
+++ Youtube Show Channel Name In Title/yt_channel_title.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube Show Channel Name In Title
 // @namespace    https://github.com/tkhquang
-// @version      1.1
+// @version      1.2
 // @description  Show channel's name (username) in title page
 // @author       Aleks
 // @license      MIT; https://raw.githubusercontent.com/tkhquang/userscripts/master/LICENSE
@@ -11,25 +11,23 @@
 // @grant        none
 // ==/UserScript==
 
-if (!String.prototype.trim) {
-    String.prototype.trim = function () {
-        return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, "");
-    };
-}
-
 (function() {
     "use strict";
 
-    var channelName, observer = new MutationObserver(setTitle);
+    var channelName;
     function setTitle() {
         if (!document.getElementById("owner-name")) {
             setTimeout(setTitle, 1000);
             return;
         }
         channelName = document.getElementById("owner-name").textContent.trim();
-        if (document.title.startsWith(channelName + " | ")) return;
+        if (document.title.startsWith(channelName + " | ")) {
+            return;
+        }
         document.title = channelName + " | " + document.title;
     }
+
+    var observer = new MutationObserver(setTitle);
     document.addEventListener("yt-navigate-finish", function () {
         if (/^\/watch?/.test(window.location.pathname)) {
             observer.observe(document.getElementsByTagName("title")[0], {
@@ -46,4 +44,4 @@ if (!String.prototype.trim) {
             }
         }
     }, true);
-})();
+}());
