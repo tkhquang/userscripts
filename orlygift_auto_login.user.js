353b00e0dd30a340a9e09230d7940bd1ac3cf447
AleksT <37518707+tkhquang@users.noreply.github.com>
2018-06-06 16:23:32 +0700

    Create orlygift_auto_login.user.js

---
+++ orlygift_auto_login.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         OrlyGift Auto LogIn
+// @namespace    https://github.com/tkhquang
+// @version      0.1
+// @description  Automatically click the steam login button.
+// @author       AleksT.
+// @license      MIT; https://raw.githubusercontent.com/tkhquang/userscripts/master/LICENSE
+// @homepage     https://greasyfork.org/en/scripts/369277-orlygift-auto-login
+// @match        https://www.orlygift.com/giveaway
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+//Intended to use with this script: https://greasyfork.org/en/scripts/29634-steam-auto-sign-in
+
+(function () {
+    "use strict";
+
+    const steamLogInBtn = document.getElementsByClassName("btn-steam");
+    if (steamLogInBtn.length === 0 || steamLogInBtn[0].hidden === true) {
+        console.log("You are logged in");
+        return;
+    }
+    steamLogInBtn[0].click();
+    console.log("Auto Login Function!");
+}());
