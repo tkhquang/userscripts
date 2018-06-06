f7608d793c0b4cb7b2c53956ef963bc22b59e2d0
AleksT <37518707+tkhquang@users.noreply.github.com>
2018-06-06 14:18:24 +0700

    nimor changes and edited Author's name

--- Youtube Show Channel Name In Title/yt_channel_title.user.js
+++ Youtube Show Channel Name In Title/yt_channel_title.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Youtube Show Channel Name In Title
+// @icon         https://s.ytimg.com/yts/img/favicon-vfl8qSV2F.ico
 // @namespace    https://github.com/tkhquang
-// @version      1.21
+// @version      1.22
 // @description  Show channel's name (username) in title page
-// @author       Aleks
+// @author       AleksT.
 // @license      MIT; https://raw.githubusercontent.com/tkhquang/userscripts/master/LICENSE
 // @homepage     https://greasyfork.org/en/scripts/368421-youtube-show-channel-name-in-title
 // @match        http*://www.youtube.com/*
@@ -12,18 +13,23 @@
 // @noframes
 // ==/UserScript==
 
-(function() {
+(function () {
     "use strict";
 
     var channelName;
+    var pattn = Boolean(/^\/watch?/.test(window.location.pathname));
     function setTitle() {
-        if (!document.getElementById("owner-name")) {
-            setTimeout(function() {
-                setTitle();
-            }, 2000);
+        var ownerName = document.getElementById("owner-name");
+        if (!pattn) {
             return;
         }
-        channelName = document.getElementById("owner-name").textContent.trim();
+        if (!ownerName || ownerName.textContent.length === 0) {
+            setTimeout(function () {
+                setTitle();
+            }, 1000);
+            return;
+        }
+        channelName = ownerName.textContent.trim();
         if (document.title.startsWith(channelName + " | ")) {
             return;
         }
@@ -32,19 +38,18 @@
 
     var observer = new MutationObserver(setTitle);
     document.addEventListener("yt-navigate-finish", function () {
-        if (/^\/watch?/.test(window.location.pathname)) {
+        if (pattn) {
             observer.observe(document.getElementsByTagName("title")[0], {
                 childList: true,
                 attributes: false,
                 characterData: false,
                 subtree: false
             });
-        }
-        else {
+        } else {
             observer.disconnect();
             if (document.title.startsWith(channelName + " | ")) {
                 document.title = document.title.replace(channelName + " | ", "");
             }
         }
-    }, true);
+    }, false);
 }());
