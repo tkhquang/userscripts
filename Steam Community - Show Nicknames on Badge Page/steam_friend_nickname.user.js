a3372b8a2077a8526a49806ac69373b57a8c29aa
Quang Trinh <khacquang.trinh@gmail.com>
2023-06-07 13:38:20 +0700

    Update script

--- Steam Community - Show Nicknames on Badge Page/steam_friend_nickname.user.js
+++ Steam Community - Show Nicknames on Badge Page/steam_friend_nickname.user.js
@@ -2,8 +2,8 @@
 // @name         Steam Community - Show Nicknames on Badge Page
 // @icon         https://store.steampowered.com/favicon.ico
 // @namespace    https://github.com/tkhquang
-// @version      1.00
-// @description  Show nicknames for friends with cards on Badge Page if exists
+// @version      1.10
+// @description  Show nicknames alongside with original names for friends with cards on Badge Page if exists
 // @author       Quang Trinh
 // @license      MIT; https://raw.githubusercontent.com/tkhquang/userscripts/master/LICENSE
 // @match        *://steamcommunity.com/*/*/gamecards/*
@@ -14,27 +14,29 @@
 (function () {
   "use strict";
 
-  Array.from(document.querySelectorAll(".badge_friendwithgamecard .persona"))
-    .forEach(friend => {
-      fetch(`https://steamcommunity.com/miniprofile/${friend.getAttribute("data-miniprofile")}`)
-        .then(
-          function(response) {
-            if (response.status !== 200) {
-              console.log(`An error occured. Status Code: ${response.status}`);
-              return;
-            }
+  [...document.querySelectorAll(".badge_friendwithgamecard .persona")].forEach((friend) => {
+    fetch(`https://steamcommunity.com/miniprofile/${friend.getAttribute("data-miniprofile")}`)
+      .then(function (response) {
+        if (response.status !== 200) {
+          console.log(`An error occured. Status Code: ${response.status}`);
+          return;
+        }
 
-            response
-              .text()
-              .then(html => {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(html, "text/html");
-                friend.innerText = doc.querySelector(".persona").innerText;
-              });
+        response.text().then((html) => {
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(html, "text/html");
+          const name = doc.querySelector(".persona")?.innerText;
+          const nickname = doc.querySelector(".secondaryname")?.innerText;
+          if (nickname) {
+            friend.innerText = `${name} ${nickname}`;
+            return;
           }
-        )
-        .catch(err => {
-          console.log("Fetch Error :-S", err);
+
+          friend.innerText = name;
         });
-    });
-}());
+      })
+      .catch((err) => {
+        console.log("Fetch Error :-S", err);
+      });
+  });
+})();
